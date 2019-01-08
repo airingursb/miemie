57fed479ec850fdfaf4e98f1ef19f35bb257ec56
Airing <airing@ursb.me>
2019-01-08 09:57:38 +0800

    refactor: 使用 open-data 替换 getUserInfo

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,29 +1,6 @@
 var app = getApp()
 
 Page({
-  data: {
-    userInfo: {}
-  },
-  onLoad: function (options) {
-    // 生命周期函数--监听页面加载   
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
-    })
-  },
-  onShareAppMessage: function () {
-    // 用户点击右上角分享
-    return {
-      title: '', // 分享标题
-      desc: '', // 分享描述
-      path: '' // 分享路径
-    }
-  },
   showMyWord: function () {
     wx.showModal({
       title: '提示',
